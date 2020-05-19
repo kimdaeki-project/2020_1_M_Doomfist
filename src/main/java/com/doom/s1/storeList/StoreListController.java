6033e918ae473e111725ff5e2c47f1cd02d29f3e
yhlim12 <63095268+yhlim12@users.noreply.github.com>
2020-05-19 17:38:33 +0900

    add_search_fianal

--- src/main/java/com/doom/s1/storeList/StoreListController.java
+++ src/main/java/com/doom/s1/storeList/StoreListController.java
@@ -20,165 +20,168 @@ import com.doom.s1.storeList.reviewFile.ReviewFileVO;
 import com.doom.s1.storeList.storeMenu.StoreMenuVO;
 import com.doom.s1.storeList.tag.StoreTagVO;
 import com.doom.s1.util.Pager;
+
 @Controller
-@RequestMapping(value="/storeList/**")
+@RequestMapping(value = "/storeList/**")
 public class StoreListController {
-	
+
 	@Autowired
 	private StoreListService storeListService;
-	
+
 	@GetMapping("searchStore")
-	public ModelAndView searchStore(Pager pager)throws Exception{
+	public ModelAndView searchStore(Pager pager) throws Exception {
 		ModelAndView mv = new ModelAndView();
-		storeListService.listCheck(pager); 	
 		
-		long a = pager.getLastNum();
-		mv.addObject("last", a);
-		
+		storeListService.listCheck(pager);
+//		  
+//		  mv.addObject("listt", storeListVOs); 
+//		  mv.addObject("pager",pager);
+		  mv.setViewName("storeList/searchStore"); 
+		  long a = pager.getLastNum();
+		  System.out.println(a);
+		  mv.addObject("last", a); 
+//		  System.out.println("user : "+storeListVOs.size());
+		 
 		return mv;
 	}
-	
+
 	@GetMapping("getList")
-	public void getList(Pager pager, Model model)throws Exception{
-		List<StoreListVO> storeListVOs = storeListService.listCheck(pager); 
+	public void getList(Pager pager, Model model) throws Exception {
+		List<StoreListVO> storeListVOs = storeListService.listCheck(pager);
 		model.addAttribute("list", storeListVOs);
 	}
-	
+
 	@GetMapping("storeListChecks")
-	public ModelAndView storeListChecks(Pager pager)throws Exception{
+	public ModelAndView storeListChecks(Pager pager) throws Exception {
 		ModelAndView mv = new ModelAndView();
 		List<StoreListVO> storeListVOs = storeListService.listCheck(pager);
 		mv.addObject("vo", storeListVOs);
 		mv.addObject("pager", pager);
-		mv.setViewName("storeList/storeListChecks");
-		
+		mv.setViewName("storeList/storeSearch");
+
 		return mv;
 	}
-	
+
 	@GetMapping("storeDelete")
-	public ModelAndView storeDelete(String[] st_key)throws Exception{
+	public ModelAndView storeDelete(String[] st_key) throws Exception {
 		ModelAndView mv = new ModelAndView();
-		//배열을 List로 변환
+		// 배열을 List로 변환
 		List<String> list = Arrays.asList(st_key);
 		int result = storeListService.storeDelete(list);
 		System.out.println(result);
 		mv.addObject("result", result);
 		mv.setViewName("common/ajaxResult");
-		
+
 		return mv;
 	}
-	
-	
-	@RequestMapping(value="storeListCheck")
-	public ModelAndView storeListCheck(Pager pager, ModelAndView mv)throws Exception{
-		List<StoreListVO> storeListVOs = storeListService.listCheck(pager); 
+
+	@RequestMapping(value = "storeListCheck")
+	public ModelAndView storeListCheck(Pager pager, ModelAndView mv) throws Exception {
+		List<StoreListVO> storeListVOs = storeListService.listCheck(pager);
 		mv.addObject("vo", storeListVOs);
 		mv.addObject("pager", pager);
 		mv.setViewName("storeList/storeListCheck");
-		
+		System.out.println("admin : " + storeListVOs.size());
 		return mv;
 	}
-	
+
 	@RequestMapping(value = "storeListSelect", method = RequestMethod.GET)
-	public ModelAndView storeListSelect(long st_key) throws Exception{
+	public ModelAndView storeListSelect(long st_key) throws Exception {
 		ModelAndView mv = new ModelAndView();
-		//store 소개
+		// store 소개
 		StoreListVO storeListVO = storeListService.storeListSelect(st_key);
-		//메뉴 소개
+		// 메뉴 소개
 		List<StoreMenuVO> storeMenuVOs = storeListService.storeMenuSelect(st_key);
-		//리뷰글 출력
+		// 리뷰글 출력
 		List<StoreListVO> storeListVOs = storeListService.storeReviewSelect(st_key);
-		//리뷰글 안 사진들 출력
+		// 리뷰글 안 사진들 출력
 		List<List<ReviewFileVO>> sList = new ArrayList<List<ReviewFileVO>>();
-		
-		for(StoreListVO storeListVO2:storeListVOs) {
+
+		for (StoreListVO storeListVO2 : storeListVOs) {
 			List<ReviewFileVO> reviewFileVOs = storeListService.reviewFileSelect(storeListVO2.getRe_num());
-			
+
 			sList.add(reviewFileVOs);
 		}
-		
-		//store 사진 출력
+
+		// store 사진 출력
 		List<StoreFileVO> storeFileVOs = storeListService.storeFileSelect(st_key);
-		
-		//리뷰 평점 평균 계산
-		double sum=0.0;
-		double avg=0.0;
-		int check=0;
+
+		// 리뷰 평점 평균 계산
+		double sum = 0.0;
+		double avg = 0.0;
+		int check = 0;
 		for (StoreListVO storeListVO2 : storeListVOs) {
 			check++;
-			sum = sum+storeListVO2.getRe_rating();
+			sum = sum + storeListVO2.getRe_rating();
 		}
-		avg = sum/check;
-		avg = Math.round(avg*10);
-		avg = avg/10.0;
-		
-		//태그 출력
+		avg = sum / check;
+		avg = Math.round(avg * 10);
+		avg = avg / 10.0;
+
+		// 태그 출력
 		List<StoreTagVO> storeTagVOs = storeListService.storeTagSelect(st_key);
-		
-	
-		mv.addObject("vo",storeListVO);		//store 소개
-		mv.addObject("vo_sm", storeMenuVOs);//메뉴 소개
-		mv.addObject("vor",storeListVOs);	//리뷰글 출력
-		mv.addObject("vof1", sList);		//리뷰 글 안 사진들 출력
-		mv.addObject("stfile", storeFileVOs);//store 사진 출력
-		mv.addObject("avg", avg);			//평점 평균출력
-		mv.addObject("vo_tag", storeTagVOs);//태그 출력
+
+		mv.addObject("vo", storeListVO); // store 소개
+		mv.addObject("vo_sm", storeMenuVOs);// 메뉴 소개
+		mv.addObject("vor", storeListVOs); // 리뷰글 출력
+		mv.addObject("vof1", sList); // 리뷰 글 안 사진들 출력
+		mv.addObject("stfile", storeFileVOs);// store 사진 출력
+		mv.addObject("avg", avg); // 평점 평균출력
+		mv.addObject("vo_tag", storeTagVOs);// 태그 출력
 		mv.setViewName("storeList/storeListSelect");
 		return mv;
 	}
-	
+
 	@GetMapping("storeReviewWrite")
-	public ModelAndView storeReviewWrite(long st_key)throws Exception{
+	public ModelAndView storeReviewWrite(long st_key) throws Exception {
 		ModelAndView mv = new ModelAndView();
 		StoreListVO storeListVO = storeListService.storeListSelect(st_key);
-		mv.addObject("vo",storeListVO);
+		mv.addObject("vo", storeListVO);
 		mv.setViewName("storeList/storeReviewWrite");
-		
+
 		return mv;
 	}
-	
 
 	@PostMapping("storeReviewWrite")
-	public ModelAndView storeReviewWrite(StoreListVO storeListVO, MultipartFile[] files)throws Exception{
+	public ModelAndView storeReviewWrite(StoreListVO storeListVO, MultipartFile[] files) throws Exception {
 		ModelAndView mv = new ModelAndView();
-	
-		long result  = storeListService.storeReviewWrite(storeListVO, files);
-		
-		String msg ="작성 실패";
-		String path="";
-		
-		if(result>0) {
+
+		long result = storeListService.storeReviewWrite(storeListVO, files);
+
+		String msg = "작성 실패";
+		String path = "";
+
+		if (result > 0) {
 			msg = "작성 성공";
-			path="./storeListSelect?st_key="+storeListVO.getSt_key();
+			path = "./storeListSelect?st_key=" + storeListVO.getSt_key();
 		}
-		
+
 		mv.addObject("result", msg);
 		mv.addObject("path", path);
 		mv.setViewName("common/result");
-		
+
 		return mv;
 	}
-	
+
 	@GetMapping("storeReviewDelete")
-	public ModelAndView storeReviewDelete(long st_key,long re_num)throws Exception{
+	public ModelAndView storeReviewDelete(long st_key, long re_num) throws Exception {
 		ModelAndView mv = new ModelAndView();
-		
+
 		long result = storeListService.storeReviewDelete(re_num);
-		
-		String msg="삭제되지 않았습니다.";
-		String path="";
-		
-		if(result>0) {
-			msg="삭제되었습니다.";
-			path="./storeListSelect?st_key="+st_key;
+
+		String msg = "삭제되지 않았습니다.";
+		String path = "";
+
+		if (result > 0) {
+			msg = "삭제되었습니다.";
+			path = "./storeListSelect?st_key=" + st_key;
 		}
-		
+
 		mv.addObject("result", msg);
 		mv.addObject("path", path);
 		mv.setViewName("common/result");
-		
+
 		return mv;
 	}
-	
-	
+
 }
